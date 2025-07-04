9e4654b22438735c09a575a00984a6bfb0e0a84a
chcorophyll <chcorophyll@gmail.com>
2025-07-04 10:32:47 +0800

    add backend data and delete mock data

--- haikou-toilet-web/src/components/Map.tsx
+++ haikou-toilet-web/src/components/Map.tsx
@@ -1,12 +1,12 @@
 // src/components/Map.tsx
-'use client'; // 声明这是一个客户端组件
+'use client';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { Toilet } from '@/data/mock-toilets'; // 引入我们的类型
+import { Toilet } from '@/data/mock-toilets';
 import L from 'leaflet';
+import { useEffect } from 'react';
 
-// Leaflet 的默认图标可能会丢失，我们手动修复它
 const defaultIcon = new L.Icon({
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
@@ -16,26 +16,30 @@ const defaultIcon = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
 });
-
 L.Marker.prototype.options.icon = defaultIcon;
 
+// 一个内部组件，用于当地图中心点变化时，平滑移动地图视野
+const ChangeView = ({ center }: { center: [number, number] }) => {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(center, 14); // 第二个参数是缩放级别
+  }, [center, map]);
+  return null;
+}
 
 interface MapProps {
   toilets: Toilet[];
+  center: [number, number]; // 接收一个中心点坐标
 }
 
-const Map: React.FC<MapProps> = ({ toilets }) => {
-  const position: [number, number] = [20.04, 110.32]; // 海口市中心大致纬度, 经度
-
+const Map: React.FC<MapProps> = ({ toilets, center }) => {
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
-      {/* 使用深色地图瓦片，以匹配我们的 Nike 风格设计 */}
+    <MapContainer center={center} zoom={14} style={{ height: '100%', width: '100%' }}>
+      <ChangeView center={center} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
       />
-      
-      {/* 遍历厕所数据，在地图上创建标记点 */}
       {toilets.map(toilet => (
         <Marker key={toilet._id} position={[toilet.location.coordinates[1], toilet.location.coordinates[0]]}>
           <Popup>
